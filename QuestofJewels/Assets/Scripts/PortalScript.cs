8a6168bcb5251a2a223b08fa0635f5e3b7de3a15
jtrev <jtrev23@gmail.com>
2020-07-23 16:36:44 -0400

    Added portal to TJayScene.unity

--- QuestofJewels/Assets/Scripts/PortalScript.cs
+++ QuestofJewels/Assets/Scripts/PortalScript.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Gamekit2D;
 public class PortalScript : MonoBehaviour
 {
-    private CharacterController thePlayerCharacter = null;
+    private PlayerCharacter thePlayerCharacter = null;
     [SerializeField]
     private bool isPortalOpen = false;
     [SerializeField]
     private string transitionScene;
+    [SerializeField]
+    private Vector3 myDepositLocation;
+    [SerializeField]
+    private static Vector3 lastDepositLocation;
+    [SerializeField]
+    private static bool transitioning = false;
 
     public bool IS_PORTAL_OPEN
     {
@@ -24,18 +31,29 @@ public class PortalScript : MonoBehaviour
 
     private void Start()
     {
-        thePlayerCharacter = GameObject.FindObjectOfType<CharacterController>();
+        thePlayerCharacter = GameObject.FindObjectOfType<PlayerCharacter>();
     }
     public void OpenPortal()
     {
-
+      
     }
 
     public void ClosePortal()
     {
 
     }
-
+    private void Awake()
+    {
+        if(transitioning == true)
+        {
+            thePlayerCharacter = GameObject.FindObjectOfType<PlayerCharacter>();
+            if(thePlayerCharacter != null)
+            {
+                thePlayerCharacter.transform.position = lastDepositLocation;
+            }
+            transitioning = false;
+        }
+    }
     private void PortalChanged()
     {
         if (isPortalOpen == true)
@@ -53,18 +71,28 @@ public class PortalScript : MonoBehaviour
         Scene nextScene = SceneManager.GetSceneByName(newScene);
         if(nextScene != null)
         {
+            lastDepositLocation = myDepositLocation;
+            transitioning = true;
             SceneManager.LoadScene(newScene, LoadSceneMode.Single);
         }
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.O))
+        if(Input.GetKeyDown(KeyCode.N))
         {
             if(thePlayerCharacter != null)
             {
-
+                if(isPortalOpen == true)
+                {
+                    if(Vector3.Distance(thePlayerCharacter.transform.position, transform.position) < 3)
+                    {
+                        TransitionTo(transitionScene);
+                    }
+                }
             }
         }
     }
+
+
 }
