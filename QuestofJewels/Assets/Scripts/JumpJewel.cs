d26fadc39dcd743082628e2b1557ca062e2ead38
ShavaughnAD <shavaughnd1@gmail.com>
2020-10-07 12:27:31 -0400

    Added Jump & Speed Power Ups & Disabled Options Menu active on Pause Menu

---
+++ QuestofJewels/Assets/Scripts/JumpJewel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Gamekit2D;
+
+public class JumpJewel : MonoBehaviour
+{
+    [Range(1, 10)]
+    [SerializeField] float jumpIncrease = 3;
+    [Range(1, 10)]
+    [SerializeField] float jumpBoostDuration = 5;
+
+    float defaultJumpSpeed;
+    PlayerCharacter playerCharacter;
+    BoxCollider2D col;
+    SpriteRenderer rend;
+    bool startCountdown = false;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (startCountdown)
+        {
+            jumpBoostDuration -= 1 * Time.deltaTime;
+            if (jumpBoostDuration <= 0)
+            {
+                playerCharacter.jumpSpeed = defaultJumpSpeed;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rend.enabled = false;
+            col.enabled = false;
+            playerCharacter = collision.gameObject.GetComponent<PlayerCharacter>();
+            defaultJumpSpeed = playerCharacter.jumpSpeed;
+            playerCharacter.jumpSpeed += jumpIncrease;
+            startCountdown = true;
+        }
+    }
+}
---
+++ QuestofJewels/Assets/Scripts/SpeedJewel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using Gamekit2D;
+
+public class SpeedJewel : MonoBehaviour
+{
+    [Range(1, 10)]
+    [SerializeField] float speedIncrease = 3;
+    [Range(1, 10)]
+    [SerializeField] float speedDuration = 5;
+    float defaultSpeed;
+
+    PlayerCharacter playerCharacter;
+    BoxCollider2D col;
+    SpriteRenderer rend;
+    bool startCountdown = false;
+
+    void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (startCountdown)
+        {
+            speedDuration -= 1 * Time.deltaTime;
+            if(speedDuration <= 0)
+            {
+                playerCharacter.maxSpeed = defaultSpeed;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            rend.enabled = false;
+            col.enabled = false;
+            playerCharacter = collision.gameObject.GetComponent<PlayerCharacter>();
+            defaultSpeed = playerCharacter.maxSpeed;
+            playerCharacter.maxSpeed += speedIncrease;
+            startCountdown = true;
+        }
+    }
+}
