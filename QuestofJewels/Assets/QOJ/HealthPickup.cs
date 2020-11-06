7978734d4d6c9c71820be97d32e0bc59685e9c4a
ShavaughnAD <shavaughnd1@gmail.com>
2020-11-06 09:19:14 -0500

    Picks Ups Scripts

---
+++ QuestofJewels/Assets/QOJ/HealthPickup.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Gamekit2D;
+
+public class HealthPickup : MonoBehaviour
+{
+    public int healAmount = 1;
+    Damageable player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Damageable>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if(player.startingHealth < 5)
+            {
+                player.startingHealth += healAmount;
+            }
+        }
+    }
+}
