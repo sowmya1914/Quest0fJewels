fff125ede67aa1c880dc994df7e3a07062b4740a
Rimour <63150407+Rimour@users.noreply.github.com>
2020-11-18 09:22:04 -0500

    Added video cutscene

---
+++ QuestofJewels/Assets/Scripts/VideoText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VideoText : MonoBehaviour
+{
+    public Text videoText;
+    public string[] FrameText;
+    
+
+    IEnumerator PlayText()
+    {
+        yield return new WaitForSeconds(2);
+        videoText.text = FrameText[0];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[1];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[2];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[3];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[4];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[5];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = FrameText[6];
+        yield return new WaitForSeconds(5.5f);
+        videoText.text = "";
+        
+
+    }
+    void Start()
+    {
+        StartCoroutine(PlayText());
+    }
+
+   
+}
